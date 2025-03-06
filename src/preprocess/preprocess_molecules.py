f06f85f5883246ea9837178ef8521c3ea38b5c90
pilsvera <pils.vera@gmail.com>
2025-03-06 21:47:54 +0100

    used morgan generator

--- src/preprocess/preprocess_molecules.py
+++ src/preprocess/preprocess_molecules.py
@@ -3,10 +3,10 @@ import numpy as np
 import pandas as pd
 from rdkit.Chem import AllChem
 from rdkit import Chem
-from rdkit.Chem import AllChem
 from rdkit.Chem import DataStructs
 #from clip import helpers
 from multiprocessing import Pool
+from rdkit.Chem import rdFingerprintGenerator 
 
 
 
@@ -14,7 +14,11 @@ def morgan_from_smiles(smiles, radius=3, nbits=1024, chiral=True):
     mol = Chem.MolFromSmiles(smiles)
     if mol is None:
         return print(f"Could not generate mol object from SMILES: {smiles}")
-    fp = AllChem.GetMorganFingerprintAsBitVect(mol, radius=3, nBits=nbits, useChirality=chiral)
+    generator = rdFingerprintGenerator.GetMorganGenerator(radius=3, fpSize=nbits, includeChirality=True)
+    fp = generator.GetFingerprint(mol)
+
+
+    #fp = AllChem.GetMorganFingerprintAsBitVect(mol, radius=3, nBits=nbits, useChirality=chiral)
     arr = np.zeros((0,), dtype=np.int8)
     DataStructs.ConvertToNumpyArray(fp,arr)
     return arr
@@ -41,7 +45,6 @@ if __name__ == '__main__':
 
     fps = map(morgan_from_smiles, smiles)
     fps = list(fps)
-    print(fps)
 
 
     #fps = helpers.parallelize(morgan_from_smiles, smiles, n_cpus)
@@ -49,6 +52,5 @@ if __name__ == '__main__':
     columns = [str(i) for i in range(fps[0].shape[0])]
 
     df = pd.DataFrame(fps, index=ids, columns=columns)
-    print(df.head())
     df.to_hdf(outfile_hdf, key="df", mode="w")
 
