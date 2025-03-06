2e88c640c050d2caa652ae2eac879edf82dcb05e
pilsvera <pils.vera@gmail.com>
2025-03-06 21:31:55 +0100

    changed preprocess molecules to fit my needs

--- src/preprocess/preprocess_molecules.py
+++ src/preprocess/preprocess_molecules.py
@@ -5,10 +5,15 @@ from rdkit.Chem import AllChem
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem import DataStructs
-from clip import helpers
+#from clip import helpers
+from multiprocessing import Pool
+
+
 
 def morgan_from_smiles(smiles, radius=3, nbits=1024, chiral=True):
     mol = Chem.MolFromSmiles(smiles)
+    if mol is None:
+        return print(f"Could not generate mol object from SMILES: {smiles}")
     fp = AllChem.GetMorganFingerprintAsBitVect(mol, radius=3, nBits=nbits, useChirality=chiral)
     arr = np.zeros((0,), dtype=np.int8)
     DataStructs.ConvertToNumpyArray(fp,arr)
@@ -16,27 +21,34 @@ def morgan_from_smiles(smiles, radius=3, nbits=1024, chiral=True):
 
 
 if __name__ == '__main__':
-    indir = "<path-to-your-folder>"
-    index = "<path-to-your-folder>/cellpainting-index.csv"
+    indir = "/home/vera/vsc5/data/AI/masterthesis/"
+    index = "source_1_named_extract.csv"
     index = os.path.join(indir, index)
 
-    outdir = "/publicwork/sanchez/data/"
+    outdir = "/home/vera/vsc5/data/AI/masterthesis/"
     outfile_hdf = "morgan_chiral_fps.hdf5"
     outfile_hdf = os.path.join(outdir, outfile_hdf)
 
-    n_cpus = 60
+    #n_cpus = 60
 
     csv = pd.read_csv(index)
 
-    csv["ID"] = csv.apply(lambda row: "-".join([str(row["PLATE_ID"]), str(row["WELL_POSITION"]),  str(row["SITE"])]), axis=1)
+    #csv["ID"] = csv.apply(lambda row: "-".join([str(row["PLATE_ID"]), str(row["WELL_POSITION"]),  str(row["SITE"])]), axis=1)
 
-    ids = csv["ID"]
+    ids = csv["Metadata_Sample_ID"]
     smiles = csv["SMILES"]
 
-    fps = helpers.parallelize(morgan_from_smiles, smiles, n_cpus)
+
+    fps = map(morgan_from_smiles, smiles)
+    fps = list(fps)
+    print(fps)
+
+
+    #fps = helpers.parallelize(morgan_from_smiles, smiles, n_cpus)
 
     columns = [str(i) for i in range(fps[0].shape[0])]
 
     df = pd.DataFrame(fps, index=ids, columns=columns)
+    print(df.head())
     df.to_hdf(outfile_hdf, key="df", mode="w")
 
